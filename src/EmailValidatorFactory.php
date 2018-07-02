38c75f5f97ca73a97fdfcec8e53902f53c2f152a
Unknown <jnk@pixelskribe.com>
2018-07-02 14:50:50 +0300

    Added functionality to choose validators

--- src/EmailValidatorFactory.php
+++ src/EmailValidatorFactory.php
@@ -17,30 +17,42 @@ class EmailValidatorFactory
 {
     /** @var Validator[] */
     private static $defaultValidators = [
-        ValidFormatValidator::class,
-        MxRecordsValidator::class,
-        MisspelledEmailValidator::class,
-        FreeEmailServiceValidator::class,
-        DisposableEmailValidator::class,
-        RoleBasedEmailValidator::class,
-        EmailHostValidator::class
+        ValidFormatValidator::class,        //0
+        MxRecordsValidator::class,          //1
+        MisspelledEmailValidator::class,    //2
+        FreeEmailServiceValidator::class,   //3
+        DisposableEmailValidator::class,    //4
+        RoleBasedEmailValidator::class,     //5
+        EmailHostValidator::class           //6
     ];
 
     /**
      * @param string $emailAddress
+     * @param array $key of validators to use [default use all]
      * @return EmailValidator
      */
-    public static function create(string $emailAddress): EmailValidator
+    public static function create(string $emailAddress, array $useValidators=[]): EmailValidator
     {
         $emailAddress = new EmailAddress($emailAddress);
         $emailDataProvider = new EmailDataProvider();
         $emailValidationResults = new ValidationResults();
         $emailValidator = new EmailValidator($emailAddress, $emailValidationResults, $emailDataProvider);
 
-        foreach (self::$defaultValidators as $validator) {
+		//$validators = self::setValidators($useValidators);
+
+        foreach (self::setValidators($useValidators) as $validator) {
             $emailValidator->registerValidator(new $validator);
         }
 
         return $emailValidator;
+    }
+
+    public static function setValidators($validators){
+        $available = self::$defaultValidators;
+        foreach($available as $key => $value){
+            if(!in_array($key,$validators))
+                unset($available[$key]);
+        }
+        return $available;
     }
 }
